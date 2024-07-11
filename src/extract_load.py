bff1a1dc83bbc724929638a6b0b321f7b8fc3799
GusFSProg <gustavo.prog40@gmail.com>
2024-07-11 16:33:07 -0300

    postgres

--- src/extract_load.py
+++ src/extract_load.py
@@ -1,15 +1,27 @@
 import pandas as pd
 import yfinance 
-# import os
+import os
 import streamlit as st
 
-# import appdirs as ad
-# ad.user_cache_dir = lambda *args: "/tmp"
-# from  sqlalchemy import create_engine
-# from  dotenv import load_dotenv
+from  sqlalchemy import create_engine
+from  dotenv import load_dotenv
+
+load_dotenv()
 
 st.set_page_config('wide')
 
+
+DB_HOST = os.getenv('DB_HOST_PROD')
+DB_PORT = os.getenv('DB_PORT_PROD')
+DB_NAME = os.getenv('DB_NAME_PROD')
+DB_USER = os.getenv('DB_USER_PROD')
+DB_PASS = os.getenv('DB_PASS_PROD')
+DB_SCHEMA = os.getenv('DB_SCHEMA_PROD')
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+
+engine = create_engine(DATABASE_URL)
 
 st.container()
 st.title("yfinance by Gustavo")
@@ -30,7 +42,21 @@ def buscar_todos_dados_commodities(commodities):
     return pd.concat(todos_dados)
 
     
+# if __name__ == "__main__":
+#         dados_concatenados = buscar_todos_dados_commodities(commodities)
+#         print(dados_concatenados)
+#         dados_concatenados
+
+        
+def salvar_no_postgres(df, schema='public'):
+    df.to_sql('commodities', engine, if_exists='replace', index=True, index_label='Date', schema=schema)
+
+    
+
 if __name__ == "__main__":
-        dados_concatenados = buscar_todos_dados_commodities(commodities)
-        print(dados_concatenados)
-        dados_concatenados
+    dados_concatenados = buscar_todos_dados_commodities(commodities)
+    salvar_no_postgres(dados_concatenados, schema='public')
+
+    dados_concatenados
+
+    
